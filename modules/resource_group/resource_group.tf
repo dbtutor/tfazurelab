b5301e02cc9f8aefe8f98c6762568bc5e96e5172
Nayan <nayanmailbox@gmail.com>
2020-04-22 22:21:25 +0530

    Create resource_group.tf

---
+++ modules/resource_group/resource_group.tf
@@ -0,0 +1,40 @@
+variable "rg" {
+    default = "terraform-lab2"
+}
+
+variable "loc" {
+    default = "West Europe"
+}
+
+variable "tags" {
+    type = map(string)
+    default = {
+        environment = "training"
+        source      = "citadel"
+    }
+}
+
+resource "azurerm_resource_group" "lab1" {
+  name     = var.rg
+  location = var.location
+  tags = var.tags
+}
+
+resource "azurerm_storage_account" "lab1sa" {
+  name                      = "richeneyterraformlab1"
+  resource_group_name       =  azurerm_resource_group.lab1.name
+  location                  =  azurerm_resource_group.lab1.location
+
+  account_kind              = "StorageV2"
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  access_tier               = "Cool"
+}
+
+output "azurerm_resource_group" {
+  value = azurerm_resource_group.lab1
+}
+
+output "azurerm_storage_account" {
+  value = azurerm_resource_group.lab1sa
+}
