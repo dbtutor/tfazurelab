410949f1d25ba1125a0ce7aa953cd5c17097fb66
Nayan <nayanmailbox@gmail.com>
2020-04-23 20:05:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+// Main.tf Block - 
+// Calling All the modules
+
 module "resource_group" {
   source = "./modules/resource_group"
+}
+
+module "app_service" {
+  source = "./modules/app_service"
 }
