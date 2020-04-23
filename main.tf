684279a297a5a1f18b7e81d95fe00c3ea60115bf
Nayan <nayanmailbox@gmail.com>
2020-04-24 01:03:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-// Main.tf Block - 
+// Main Block aka Root Module block
 // Calling All the modules
 
 module "resource_group" {
@@ -7,4 +7,7 @@ module "resource_group" {
 
 module "app_service" {
   source = "./modules/app_service"
+  # Outputs can only be passed from one module into another by way of the root module.
+  # https://www.terraform.io/docs/modules/composition.html
+  resource_group = module.resource_group.azurerm_resource_group.name
 }
