719e427efa63273dda5eb70632e521dfdedde8fa
Nayan <nayanmailbox@gmail.com>
2020-04-23 20:04:15 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+// Main Output Block
+// Syntax - module.<MODULE_NAME>.<OUTPUT_NAME>
+
+output "azurerm_resource_group" {
+    value = module.resource_group.azurerm_resource_group
+    description = "Azure Resource Group Output"
+}
+
+output "azurerm_storage_account" {
+    value  = module.resource_group.azurerm_storage_account
+    description = "Azure Storage Account Output"
+}
+
+output "azurerm_app_service" {
+    value  = module.app_service.azurerm_app_service
+    description = "Azure App Service Output"
+}
+
+
